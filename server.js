acf0886893f800d1804fd0a74c0e0913239de63b
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-06-18 13:58:56 +0700

    edit readme

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const registerRoutes = require("./routes/registerRoutes");
+const indexRoutes = require("./routes/indexRoutes");
+const loginRoutes = require("./routes/loginRoutes");
+
 const app = express();
-// const { body, validationResult } = require("express-validator");
-// const bcrypt = require("bcrypt");
-// const { db } = require("./connection");
 app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const registerRoutes = require("./routes/registerRoutes");
-const indexRoutes = require("./routes/indexRoutes");
-
 app.use(express.static("public"));
 app.use("/", indexRoutes);
 app.use("/register", registerRoutes);
+app.use("/login", loginRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
