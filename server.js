b499f3e06c0d2ceb200386368f7086586da07d5b
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-06-19 15:18:30 +0700

    replacing ejs with mustache

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mustache = require("mustache-express");
 const cors = require("cors");
 
 const app = express();
 const port = 3000;
-// const testRoutes = require("./routes/testRoutes");
-// const indexRoutes = require("./routes/indexRoutes");
-// const loginRoutes = require("./routes/loginRoutes");
 
-// const app = express();
-app.set("view engine", "ejs");
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// app.use("/", indexRoutes);
-// app.use("/register", testRoutes);
-// app.use("/login", loginRoutes);
-
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log(`App is running on port ${PORT}`);
-// });
 const userController = require("./controllers/user-controller");
 const authController = require("./controllers/auth-controller");
 
 // Sample route
-app.get('/');
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Express with Mustache' });
+});
+
 app.post('/register', userController.registerUser);
 app.post('/login', authController.loginUser);
-
 
 // Start the server
 app.listen(port, () => {
